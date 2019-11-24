dadc37bd02182d57557cc436d13f05978f795bac
Tom Banish <tombanish@gmail.com>
2019-11-24 16:27:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    "#{array[0]}"
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    
+  end
 end
