b2bfac4b3de00d42bd391e315496088ae9530f36
Tom Banish <tombanish@gmail.com>
2019-11-24 16:30:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     "#{array[0]}"
   elsif array.length == 2
-    array.join("and ")
+    array.join(" and ")
   elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
     array.join(", ")
