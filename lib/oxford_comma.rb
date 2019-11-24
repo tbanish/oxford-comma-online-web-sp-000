463823d21beb87743eee523fb464ca0c9a840beb
Tom Banish <tombanish@gmail.com>
2019-11-24 16:29:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     "#{array[0]} and #{array[1]}"
   elsif array.length >= 3
-    array[-1] = " and #{array[-1]}"
+    array[-1] = "and #{array[-1]}"
     array.join(", ")
   end
 end
